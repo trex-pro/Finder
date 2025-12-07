2334acce4b000b35d17612ebee32075f8af30771
trex-pro <mailto.trexpro@gmail.com>
2025-12-07 16:56:02 +0530

    Deleted Frontend

--- client/src/core/gaurds/unsaved-changes-guard.ts
+++
@@ -1,9 +0,0 @@
-import { CanDeactivateFn } from '@angular/router';
-import { MemberProfile } from '../../features/members/member-profile/member-profile';
-
-export const unsavedChangesGuard: CanDeactivateFn<MemberProfile> = (component: MemberProfile) => {
-  if (component.editForm?.dirty) {
-    return confirm('Continue? All unsaved changes will be lost.');
-  }
-  return true;
-};
--- client/src/types/error.ts
+++
@@ -1,5 +0,0 @@
-export type ApiError = {
-    message: string;
-    statusCode: number;
-    details? : string;
-}
